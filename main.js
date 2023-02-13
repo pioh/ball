c7f7e6ed756852626ca10c98166866e1668cff98
Artemiy Dudko <thepioh@zoho.com>
2023-02-13 21:20:59 +0300

    initial

---
+++ main.js
@@ -0,0 +1,43 @@
+
+let b = document.getElementsByClassName("ball")[0]
+let scene = document.getElementsByClassName("scene")[0]
+
+console.log(b)
+
+let y = 32, x = 32;
+let v = 10;
+let vx = 4;
+
+function step() {
+    let H = scene.clientHeight - b.clientHeight;
+    let W = scene.clientWidth - b.clientWidth;
+    v += 1 / 2;
+    x += vx;
+    y += v;
+    console.log(v)
+    let dy = y - H
+    if (dy >= 0) {
+        y -= dy * 2;
+        v *= -1;
+    }
+    if (y <= 0) {
+        y -= y * 2;
+        v *= -1
+    }
+    let dx = x - W;
+    if (dx >= 0) {
+        x -= dx * 2;
+        vx *= -1
+    }
+    if (x <= 0) {
+        x -= x * 2;
+        vx *= -1
+    }
+
+    b.style.top = y + "px"
+    b.style.left = x + "px"
+}
+
+
+
+setInterval(step, 10)
